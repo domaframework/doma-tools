0ee1e46b17bdf1f77b4463dc4a002d4be5b144b5
nakamura-to <toshihiro.nakamura@gmail.com>
2010-08-29 03:50:20 +0000

    [DOMA-204][Tools] Eclipse起動後一番最初に動かすDoma Toolsの機能がSQLファイルの修正による自動ビルドの場合に、ビルドが行われない問題に対応しました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@904 362887e2-e868-44d1-a061-b03550a0b845

--- org.seasar.doma.extension.domax/src/org/seasar/doma/extension/domax/Domax.java
+++ org.seasar.doma.extension.domax/src/org/seasar/doma/extension/domax/Domax.java
@@ -17,13 +17,14 @@ package org.seasar.doma.extension.domax;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Domax extends AbstractUIPlugin {
+public class Domax extends AbstractUIPlugin implements IStartup {
 
     public static final String PLUGIN_ID = "org.seasar.doma.extension.domax";
 
@@ -36,12 +37,15 @@ public class Domax extends AbstractUIPlugin {
     }
 
     @Override
+    public void earlyStartup() {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        workspace.addResourceChangeListener(new ResourceFileChangeListener());
+    }
+
+    @Override
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
-        
-        IWorkspace workspace = ResourcesPlugin.getWorkspace();
-        workspace.addResourceChangeListener(new ResourceFileChangeListener());
     }
 
     @Override
